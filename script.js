613783701e61c3478bacf8fe0344b77a77612385
Antonio Cunha <antonio.frango@gmail.com>
2023-01-08 21:38:13 -0300

    Exercicio - cadastro de recruta

---
+++ script.js
@@ -0,0 +1,9 @@
+//Cadastro de Recrutas
+
+let nome = 'Antonio'
+let sobrenome = 'Cunha'
+let campoDeEstudo = 'Dev Web'
+let anoNascimento = 1980
+let idade = 2023 - anoNascimento
+
+console.log('Nome completo: ' + nome + sobrenome + ' Campo de estudo: ' + campoDeEstudo + ' Idade: ' + idade);
