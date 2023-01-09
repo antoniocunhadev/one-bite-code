0706f4f556da297ea910df6d3375f199f318dbe3
Antonio Cunha <antonio.frango@gmail.com>
2023-01-08 21:43:14 -0300

    Exercicio - calculadora de 4 operacoes

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let anoNascimento = 1980
 let idade = 2023 - anoNascimento
 
 console.log('Nome completo: ' + nome + sobrenome + ' Campo de estudo: ' + campoDeEstudo + ' Idade: ' + idade);
+
+//Calculadora de 4 operações
+
+let a = 10
+let b = 2
+let soma = a + b
+let subtrair = a - b
+let multiplicar = a * b
+let divivir = a / b
+
+console.log(soma, subtrair, multiplicar, divivir);
