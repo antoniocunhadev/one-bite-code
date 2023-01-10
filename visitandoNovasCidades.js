781a81a0e31996a1c5e0e4ae42dbb8bd09795cda
Antonio Cunha <antonio.frango@gmail.com>
2023-01-10 12:04:01 -0300

    Exercicio - visitando novas cidades

---
+++ visitandoNovasCidades.js
@@ -0,0 +1,14 @@
+let turista = prompt("Qual o seu nome?");
+
+let cidades = "";
+let contagem = 0;
+
+let visitouCidades = prompt("Você visitou alguma cidade (sim/não)");
+
+while (visitouCidades === "sim") {
+  let cidade = prompt("Qual o nome da cidade?");
+  cidades += " - " + cidade + "\n";
+  contagem += 1;
+  visitouCidades = prompt("Você visitou mais alguma cidade? (sim/não)");
+}
+alert(turista + "\n" + contagem + "\n" + cidades);
