04cd0dc47d4267ad0c4db6bb01135214006d80e3
Antonio Cunha <antonio.frango@gmail.com>
2023-01-26 21:00:51 -0300

    Exercicio Dom - Escalacao do time add funcoes

--- escalacaoDoTime.js
+++ escalacaoDoTime.js
@@ -1,23 +1,37 @@
 function addJogador() {
   let jogadoresList = document.getElementById("jogadores-list");
 
-  const h3 = document.createElement("h3");
+  let h3 = document.createElement("h3");
   h3.innerText = "Jogador";
 
-  const ul = document.createElement("ul");
+  let ul = document.createElement("ul");
 
-  const posicaoLi = document.createElement("li");
+  let posicaoLi = document.createElement("li");
   posicaoLi.innerText = "Posição: ";
-  const posicaoInput = document.createElement("input");
+  let posicaoInput = document.createElement("input");
   posicaoInput.type = "text";
   posicaoInput.name = "fullname";
   posicaoLi.appendChild(posicaoInput);
   ul.appendChild(posicaoLi);
   ul.appendChild(document.createElement("br"));
 
-  
+  let nomeLi = document.createElement("li");
+  nomeLi.innerText = "Nome: ";
+  let nomeInput = document.createElement("input");
+  nomeInput.type = "text";
+  nomeInput.name = "fullname";
+  nomeLi.appendChild(nomeInput);
+  ul.appendChild(nomeLi);
+  ul.appendChild(document.createElement("br"));
+
+  let camisaLi = document.createElement("li");
+  camisaLi.innerText = "Numero da camisa: ";
+  let camisaInput = document.createElement("input");
+  camisaInput.type = "text";
+  camisaInput.name = "fullname";
+  camisaLi.appendChild(camisaInput);
+  ul.appendChild(camisaLi);
+  ul.appendChild(document.createElement("br"));
 
   jogadoresList.append(h3, ul);
 }
-
-
