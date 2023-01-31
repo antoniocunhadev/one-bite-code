1cc466f0400b7dc930ab88f0098cba862c909514
Antonio Cunha <antonio.frango@gmail.com>
2023-01-30 21:33:36 -0300

    Exercicio Dom - Cadastro de Desenvolvedores

---
+++ cadastroDeDevs.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("cadastroForm");
+
+form.addEventListener("submit", function (addTecnologia) {
+  addTecnologia.preventDefault();
+  const nome = document.getElementById("name").value;
+  const tecnologia = document.getElementById("tecnologia").value;
+  const tempo = document.getElementsByName("experiencia").value;
+
+  const confirmation = confirm(
+    "Add " + tecnologia + " com " + tempo + " anos de experiência?"
+  );
+
+  if (confirmation) {
+    const tecnologiaList = document.getElementById("tecnologia-list");
+    const tecnologiaItem = document.createElement("li");
+    tecnologiaItem.id = "tecnologia-" + tecnologia;
+    tecnologiaItem.innerText = nome + " : " + tecnologia;
+    tecnologiaList.appendChild(tecnologiaItem);
+
+    document.getElementById("name").value = "";
+    document.getElementById("tecnologia").value = "";
+  }
+});
